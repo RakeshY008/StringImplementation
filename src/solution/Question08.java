2f6f918bdc0d9a9778b47ae62ba43626f4bbddd5
RakeshY008 <rakesh.yadav_cs18@gla.ac.in>
2019-10-02 17:01:59 +0530

    Question08 main method added to take input by user in any case

---
+++ src/solution/Question08.java
@@ -0,0 +1,15 @@
+package solution;
+import java.util.Scanner;
+
+/**
+ * @ Author : Rakesh Yadav
+ */
+
+public class Question08 {
+    public static void main(String[] artgs) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Enter String Value : ");
+        String stringInput = scanner.nextLine();
+
+    }
+}
