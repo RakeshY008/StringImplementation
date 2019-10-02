4fdd7810606d70265cd9a9c82ec1a4721c6a2cc5
RakeshY008 <rakesh.yadav_cs18@gla.ac.in>
2019-10-02 17:58:27 +0530

    Quetion11: init commit

--- src/solution/Question10.java
+++ src/solution/Question10.java
@@ -8,8 +8,11 @@ import java.util.Scanner;
 public class Question10 {
     public static void main(String[] artgs) {
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("Enter String Value : ");
-        String stringInput = scanner.nextLine();
+        System.out.printf("ch  =  ");
+        char ch = Character.isJavaIdentifierPart(System.setIn(););
+        System.out.printf("inputString = ");
+        String inputString = scanner.next();
+        System.out.println(inputString.charAt(Integer.parseInt(ch)));
 
     }
 }
---
+++ src/solution/Question11.java
@@ -0,0 +1,13 @@
+package solution;
+import java.util.Scanner;
+
+/**
+ * @ Author : Rakesh Yadav
+ */
+
+public class Question11 {
+    public static void main(String[] artgs) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
