50f5db5d03fc06e5f2df4a4ba9304454b1bcba44
RakeshY008 <rakesh.yadav_cs18@gla.ac.in>
2019-10-02 16:27:16 +0530

    Question06 : convert inputString into lowerCase
                                init Commit

---
+++ src/solution/Question06.java
@@ -0,0 +1,17 @@
+package solution;
+import java.util.Scanner;
+
+/**
+ * @ Author : Rakesh Yadav
+ */
+
+public class Question06 {
+    public static void main(String[] artgs){
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Enter String Value : ");
+        String stringInput = scanner.nextLine();
+
+
+    }
+}
+
