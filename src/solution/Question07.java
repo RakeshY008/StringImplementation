41b089703c72ab125db2be5cfd4ba9b63839383a
RakeshY008 <rakesh.yadav_cs18@gla.ac.in>
2019-10-02 16:33:16 +0530

    Question07: main method

---
+++ src/solution/Question07.java
@@ -0,0 +1,14 @@
+package solution;
+import java.util.Scanner;
+
+/**
+ * @ Author : Rakesh Yadav
+ */
+
+public class Question07 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String stringInput = scanner.nextLine();
+
+    }
+}
