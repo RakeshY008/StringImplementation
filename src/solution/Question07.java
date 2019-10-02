27e08952a30d33889beb0f79c929cf4fceba49ca
RakeshY008 <rakesh.yadav_cs18@gla.ac.in>
2019-10-02 16:42:54 +0530

    Question07: method to calculate totalWeight of given string value

--- src/solution/Question07.java
+++ src/solution/Question07.java
@@ -9,6 +9,13 @@ public class Question07 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String stringInput = scanner.nextLine();
+        int lengthOfString = stringInput.length();
+        int totalWeight = 0;
+        for (int i = 0; i < lengthOfString ; i++) {
+            totalWeight = totalWeight + (int) stringInput.charAt(i);
+
+        }
+        System.out.println("TotalWeight String : "+ totalWeight);
 
     }
 }
