439fa41ee470a315815d5ed91934aebaa68a961c
RakeshY008 <rakesh.yadav_cs18@gla.ac.in>
2019-10-02 17:29:11 +0530

    Question09:  added calculation for absoluteWeight = UpperWeight - LowerWeight              using for if condition within for loop                  final commit

--- src/solution/Question09.java
+++ src/solution/Question09.java
@@ -11,5 +11,19 @@ public class Question09 {
         System.out.printf("Enter String Value : ");
         String stringInput = scanner.nextLine();
 
+        int LowerWeight = 0, UpperWeight = 0 ,absoluteWeight =0;
+
+        for (int i = 0; i <stringInput.length() ; i++) {
+            if (stringInput.charAt(i) >= 'A' && stringInput.charAt(i) <= 'Z'){
+                UpperWeight += (int) stringInput.charAt(i);
+            }
+            else if (stringInput.charAt(i) >= 'a' || stringInput.charAt(i) <= 'z'){
+                LowerWeight += (int) stringInput.charAt(i);
+            }
+        }
+        absoluteWeight = UpperWeight - LowerWeight;
+        if (absoluteWeight < 0)
+            System.out.println("absoluteWeight = " + absoluteWeight*(-1));
+        else System.out.println("absoluteWeight = " + absoluteWeight);
     }
 }
