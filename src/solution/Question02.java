d92af06d41267cd96d6f75f0ad3a6e85d36b63dd
RakeshY008 <rakesh.yadav_cs18@gla.ac.in>
2019-10-02 14:46:41 +0530

    Question02 ...  final commit

--- src/solution/Question02.java
+++ src/solution/Question02.java
@@ -8,6 +8,15 @@ import java.util.Scanner;
 public class Question02 {
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
 
+        inputString = inputString.toLowerCase();
+        inputString = inputString.replace('a','*');
+        inputString = inputString.replace('e','*');
+        inputString = inputString.replace('i','*');
+        inputString = inputString.replace('o','*');
+        inputString = inputString.replace('u','*');
+
+        System.out.println(inputString);
     }
 }
